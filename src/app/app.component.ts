9f3b9ac41e0b78b1e93a94ca767282bf07cd972b
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-11-24 15:18:22 +0300

    Define 'searchName' function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GitHub User Lookup';
+
+  constructor(private http: Http){}
+
+  userName = '';
+  searchName() {
+  this.http.get('https://api.github.com/users' + this.userName)
+}
+
 }
