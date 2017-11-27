c72d7d758b594072e52e15e0c9c66e22dc7d1229
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-11-27 09:18:26 +0300

    Modify 'repos_url'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
-// import { apiKey } from '.../env'
+// import { apiKey } from '../env';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +14,29 @@ export class AppComponent {
 
   userName = '';
   githubInfo: any = '';
+  // public_repos = '';
+
   searchName() {
   this.http.get('https://api.github.com/users/' + this.userName)
   .subscribe (
     (res:Response) => {
       const githubUser = res.json();
       this.githubInfo=githubUser;
+      // this.public_repos=githubUser;
       console.log(githubUser);
     }
   )
 }
+// searchPublicRepos() {
+//   this.http.get('res.repos_url')
+//   // this.http.get('https://api.github.com/users/repos' + this.public_repos)
+//   .subscribe (
+//     (res:Response) => {
+//       const githubUserRepo = res.json();
+//       this.public_repos=githubUserRepo;
+//       console.log(githubUserRepo);
+//     }
+//   )
+// }
 
 }
